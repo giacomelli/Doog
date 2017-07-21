402eb2ab199786365c84fe069f3342414af9ea31
Adriano Viana <adrianoviana@cwi.com.br>
2017-07-21 19:58:38 -0300

    initial commit

---
+++ src/Snake/Game/SnakeGame.cs
@@ -0,0 +1,121 @@
+﻿using System;
+
+namespace Snake.Game
+{
+    public class SnakeGame : IDisposable
+    {
+        private SnakeTile head;
+        private SnakeTile tail;
+        private int movingSpeed;
+        private int movingDirectionX;
+        private int movingDirectionY;
+
+        public void Initialize()
+        {
+            Console.CursorVisible = false;
+            movingSpeed = 1;
+            movingDirectionX = movingSpeed;
+            movingDirectionY = 0;
+
+            CreateSnake(10, 10, 20);
+        }
+
+        public void Update()
+        {
+            HandleUserInput();
+            Move();
+        }
+
+        public void Draw()
+        {
+            Console.Clear();
+            SnakeTile temp = tail;
+            while (temp != null)
+            {
+                Console.SetCursorPosition(temp.X, temp.Y);
+                Console.Write('@');
+                temp = temp.Next;
+            }
+        }
+
+        public void Dispose()
+        {
+        }
+
+        private void Move()
+        {
+            tail.CopyPosition(head);
+            tail.X += movingDirectionX;
+            tail.Y += movingDirectionY;
+            head.Next = tail;
+            head = tail;
+            tail = tail.Next;
+            head.Next = null;
+        }
+
+        private void CreateSnake(int x, int y, int length)
+        {
+            if (length < 3)
+            {
+                throw new ArgumentException("length must be greater than 2", "length");
+            }
+
+            tail = new SnakeTile(x++, y);
+            tail.Next = new SnakeTile(x++, y);
+            head = new SnakeTile(x++ + 2, y);
+            tail.Next.Next = head;
+            length -= 3;
+
+            for (int i = 0; i < length; i++, x++)
+            {
+                head.Next = new SnakeTile(x, y);
+                head = head.Next;
+            }
+        }
+
+        private void HandleUserInput()
+        {
+            if (Console.KeyAvailable)
+            {
+                switch (Console.ReadKey(true).Key)
+                {
+                    case ConsoleKey.LeftArrow:
+                    case ConsoleKey.H:
+                        if (movingDirectionX == 0)
+                        {
+                            movingDirectionX = -movingSpeed;
+                            movingDirectionY = 0;
+                        }
+                        break;
+
+                    case ConsoleKey.UpArrow:
+                    case ConsoleKey.K:
+                        if (movingDirectionY == 0)
+                        {
+                            movingDirectionY = -movingSpeed;
+                            movingDirectionX = 0;
+                        }
+                        break;
+
+                    case ConsoleKey.RightArrow:
+                    case ConsoleKey.L:
+                        if (movingDirectionX == 0)
+                        {
+                            movingDirectionX = movingSpeed;
+                            movingDirectionY = 0;
+                        }
+                        break;
+
+                    case ConsoleKey.DownArrow:
+                    case ConsoleKey.J:
+                        if (movingDirectionY == 0)
+                        {
+                            movingDirectionY = movingSpeed;
+                            movingDirectionX = 0;
+                        }
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ src/Snake/Game/SnakeTile.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Snake.Game
+{
+    public class SnakeTile : IEnumerable<SnakeTile>, IEnumerator<SnakeTile>
+    {
+        private SnakeTile enumeratorCurrent;
+
+        public int X { get; set; }
+
+        public int Y { get; set; }
+
+        public SnakeTile Next { get; set; }
+
+        public SnakeTile(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public void CopyPosition(SnakeTile other)
+        {
+            X = other.X;
+            Y = other.Y;
+        }
+
+        public IEnumerator<SnakeTile> GetEnumerator()
+        {
+            return this;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this;
+        }
+
+        bool IEnumerator.MoveNext()
+        {
+            if (enumeratorCurrent == null)
+            {
+                enumeratorCurrent = this;
+            }
+            else
+            {
+                enumeratorCurrent = enumeratorCurrent.Next;
+            }
+
+            return enumeratorCurrent.Next != null;
+        }
+
+        SnakeTile IEnumerator<SnakeTile>.Current
+        {
+            get
+            {
+                return enumeratorCurrent;
+            }
+        }
+
+        object IEnumerator.Current
+        {
+            get
+            {
+                return enumeratorCurrent;
+            }
+        }
+
+        void IEnumerator.Reset()
+        {
+            enumeratorCurrent = this;
+        }
+
+        #region IDisposable Support
+
+        private bool disposedValue = false; // To detect redundant calls
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    enumeratorCurrent = null;
+                }
+
+                disposedValue = true;
+            }
+        }
+
+        // TODO: override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources.
+        // ~SnakeTile() {
+        //   // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+        //   Dispose(false);
+        // }
+
+        // This code added to correctly implement the disposable pattern.
+        void IDisposable.Dispose()
+        {
+            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+            Dispose(true);
+            // TODO: uncomment the following line if the finalizer is overridden above.
+            // GC.SuppressFinalize(this);
+        }
+
+        #endregion IDisposable Support
+    }
+}
