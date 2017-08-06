5a559efee01cff2720e56c3fb42bd30053b3a3a5
Diego Giacomelli <giacomelli@gmail.com>
2017-08-06 11:29:03 -0300

    Using the text system on game

--- src/Snake/Game/SnakeGame.cs
+++ src/Snake/Game/SnakeGame.cs
@@ -5,104 +5,116 @@ using Snake.Framework;
 using Snake.Framework.Behaviors;
 using Snake.Framework.Graphics;
 using Snake.Framework.Physics;
+using Snake.Framework.Texts;
 
 namespace Snake.Game
 {
-    public class SnakeGame : IDisposable
-    {
+	public class SnakeGame : IDisposable
+	{
 		private const int MaxSnakes = 1;
-        private IWorld world;        
-        private Snake[] snakes;
-        private bool gameOver;
-        public void Initialize(IGraphicSystem graphicSystem)
-        {
-			world = new World(graphicSystem, new PhysicSystem());
-			
-            // Create the snakes.
-            snakes = new Snake[MaxSnakes];
+		private IWorld world;
+		private Snake[] snakes;
+		private bool gameOver;
+		private IGraphicSystem graphicSystem;
+
+		public void Initialize(IGraphicSystem graphicSystem, ITextSystem textSystem)
+		{
+			this.graphicSystem = graphicSystem;
+			world = new World(graphicSystem, new PhysicSystem(), textSystem);
+
+			// Create the snakes.
+			snakes = new Snake[MaxSnakes];
 
 			for (int i = 0; i < MaxSnakes; i++)
 			{
 				var snake = new Snake();
-				snake.Initialize(0, 10 + i, 6, graphicSystem.Bounds, world);
-            	snakes[i] = snake;
+				snake.Initialize(0, 10 + i, 20, graphicSystem.Bounds, world);
+				snakes[i] = snake;
 
-                world.AddComponent(snake);
+				world.AddComponent(snake);
 			}
 
-            // Create the food spawner.
-            world.AddComponent(new FoodSpawner(graphicSystem.Bounds, world));
+			// Create the food spawner.
+			world.AddComponent(new FoodSpawner(graphicSystem.Bounds, world));
 
-            gameOver = false;
-        }
+			gameOver = false;
+		}
 
-        public bool GameOver
-        {
-            get
-            {
-                return gameOver;
-            }
-        }
+		public bool GameOver
+		{
+			get
+			{
+				return gameOver;
+			}
+		}
 
-        public void Update()
-        {
-            CheckGameOver();
+		public void Update()
+		{
+			CheckGameOver();
 
-            if (!gameOver)
-            {
-                world.Update();
-            }           
-        }
+			if (gameOver)
+			{
+				world.TextSystem.DrawCenter("Game over", graphicSystem.Bounds);
+				world.TextSystem.DrawCenter(0, 7, "Score: "+  snakes[0].FoodsEatenCount, graphicSystem.Bounds, "Default");
+			}
+			else
+			{
+				world.TextSystem.DrawCenterX(1, "Doog's Snake", graphicSystem.Bounds);
+				world.TextSystem.DrawCenterX(7, "Score: "+  snakes[0].FoodsEatenCount, graphicSystem.Bounds, "Default");
 
-        public void Draw()
-        {
-            world.Draw();
-        }
+				world.Update();
+			}
+		}
 
-        private bool disposed = false; // To detect redundant calls
+		public void Draw()
+		{
+			world.Draw();
+		}
 
-        protected virtual void Dispose(bool disposing)
-        {
-            if (!disposed)
-            {
-                if (disposing)
-                {
-                }
+		private bool disposed = false; // To detect redundant calls
 
-                disposed = true;
-            }
-        }
+		protected virtual void Dispose(bool disposing)
+		{
+			if (!disposed)
+			{
+				if (disposing)
+				{
+				}
 
-        // TODO: override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources.
-        // ~SnakeGame() {
-        //   // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
-        //   Dispose(false);
-        // }
+				disposed = true;
+			}
+		}
 
-        // This code added to correctly implement the disposable pattern.
-        void IDisposable.Dispose()
-        {
-            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
-            Dispose(true);
-            // TODO: uncomment the following line if the finalizer is overridden above.
-            // GC.SuppressFinalize(this);
-        }
+		// TODO: override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources.
+		// ~SnakeGame() {
+		//   // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+		//   Dispose(false);
+		// }
 
-        private void CheckGameOver()
-        {
-            if (!gameOver)
-            {
-                for (int i = 0; i < MaxSnakes; i++)
-                {
-                    var snake = snakes[i];
+		// This code added to correctly implement the disposable pattern.
+		void IDisposable.Dispose()
+		{
+			// Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+			Dispose(true);
+			// TODO: uncomment the following line if the finalizer is overridden above.
+			// GC.SuppressFinalize(this);
+		}
 
-                    if(snake.IsOutOfBounds() || snake.Dead)
-                    {
-                        gameOver = true;
-                        break;
-                    }
-                }                
-            }
-        }
-    }
+		private void CheckGameOver()
+		{
+			if (!gameOver)
+			{
+				for (int i = 0; i < MaxSnakes; i++)
+				{
+					var snake = snakes[i];
+
+					if (snake.IsOutOfBounds() || snake.Dead)
+					{
+						gameOver = true;
+						break;
+					}
+				}
+			}
+		}
+	}
 }
