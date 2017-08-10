3554c1ecc450f41b08e4667cec9b59b078063b8e
giacomelli <giacomelli@gmail.com>
2017-08-10 20:30:02 -0300

    Improved the code coverage

---
+++ src/Snake.Framework.UnitTests/Diagnostics/DebugTest.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using System;
+using Smocks;
+
+namespace Snake.Framework.UnitTests.Diagnostics
+{
+    [TestFixture]
+    public class DebugTest
+    {
+        [Test]
+        public void Initialize_NoDebugArg_EnabledFalse()
+        {
+            Debug.Initialize("cmd.exe", "test");
+            Assert.IsFalse(Debug.Enabled);		
+        }
+
+		[Test]
+		public void Enabled_DebugArg_True()
+		{
+            Debug.Initialize("cmd.exe", "Debug");
+            Assert.IsTrue(Debug.Enabled);
+		}
+    }
+}
---
+++ src/Snake.Framework.UnitTests/WorldContextExtensionsTest.cs
@@ -0,0 +1,62 @@
+﻿using NUnit.Framework;
+using System;
+using Rhino.Mocks;
+using Snake.Framework.Behaviors;
+using System.Collections.Generic;
+
+namespace Snake.Framework.UnitTests
+{
+    [TestFixture]
+    public class WorldContextExtensionsTest
+    {
+        [Test]
+        public void RemoveComponentsWithoutTag_Tag_KeepOnlyComponentsWithTag()
+        {
+            var c1 = MockRepository.GenerateMock<IComponent>();
+            c1.Expect(c => c.Tag).Return("Tag1");
+
+            var c2 = MockRepository.GenerateMock<IComponent>();
+            c2.Expect(c => c.Tag).Return("Tag2");
+
+            var c3 = MockRepository.GenerateMock<IComponent>();
+            c3.Expect(c => c.Tag).Return("Tag1");
+
+            var ctx = MockRepository.GenerateMock<IWorldContext>();
+            ctx.Expect(c => c.Components).Return(new List<IComponent>(new IComponent[] {
+                c1, c2, c3
+            }));
+            ctx.Expect(c => c.RemoveComponent(c1));
+            ctx.Expect(c => c.RemoveComponent(c3));
+
+            ctx.RemoveComponentsWithoutTag(("Tag2"));
+
+            c1.VerifyAllExpectations();
+            c2.VerifyAllExpectations();
+            c3.VerifyAllExpectations();
+            ctx.VerifyAllExpectations();
+        }
+
+		[Test]
+		public void RemoveAllComponents_NoArgs_AllComponentsRemoved()
+		{
+			var c1 = MockRepository.GenerateMock<IComponent>();
+			var c2 = MockRepository.GenerateMock<IComponent>();
+			var c3 = MockRepository.GenerateMock<IComponent>();
+		
+			var ctx = MockRepository.GenerateMock<IWorldContext>();
+			ctx.Expect(c => c.Components).Return(new List<IComponent>(new IComponent[] {
+				c1, c2, c3
+			}));
+			ctx.Expect(c => c.RemoveComponent(c1));
+            ctx.Expect(c => c.RemoveComponent(c2));
+			ctx.Expect(c => c.RemoveComponent(c3));
+
+			ctx.RemoveAllComponents();
+
+			c1.VerifyAllExpectations();
+			c2.VerifyAllExpectations();
+			c3.VerifyAllExpectations();
+			ctx.VerifyAllExpectations();
+		}
+    }
+}
