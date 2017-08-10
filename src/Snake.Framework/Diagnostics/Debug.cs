a1b625b1d6e728a09a0b1408ed4be9f00af8824a
giacomelli <giacomelli@gmail.com>
2017-08-09 22:16:46 -0300

    Added Debug helper class

---
+++ src/Snake.Framework/Diagnostics/Debug.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Snake.Framework
+{
+	/// <summary>
+	/// Central point to debug stuffs.
+	/// </summary>
+	public static class Debug
+	{
+		static Debug()
+		{
+			var args = Environment.GetCommandLineArgs();
+
+			// Debug mode can be enable by passing the parameter "debug" to the game executable.
+			Enabled = args.Length > 1 && args[1].Equals("debug", StringComparison.OrdinalIgnoreCase);
+		}
+
+		/// <summary>
+		/// Gets a value indicating whether debug mode is enabled.
+		/// </summary>
+		/// <value><c>true</c> if enabled; otherwise, <c>false</c>.</value>
+		public static bool Enabled { get; private set; }
+	}
+}
