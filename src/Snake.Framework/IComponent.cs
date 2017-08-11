b839ad7053d70675abd63b8fddb12a49be897428
giacomelli <giacomelli@gmail.com>
2017-08-11 18:47:11 -0300

    Code doc improved

--- src/Snake.Framework/IComponent.cs
+++ src/Snake.Framework/IComponent.cs
@@ -1,10 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace Snake.Framework
+﻿namespace Snake.Framework
 {
+    /// <summary>
+    /// Define an interface for components.
+    /// </summary>
     public interface IComponent
     {
         bool Enabled { get; set; }
