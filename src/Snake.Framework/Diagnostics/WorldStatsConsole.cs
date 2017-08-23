5c780abb4e98ad0e6f82c5e61c99b8e9d5fba524
giacomelli <giacomelli@gmail.com>
2017-08-22 22:10:40 -0300

    Added FPS

--- src/Snake.Framework/Diagnostics/WorldStatsConsole.cs
+++ src/Snake.Framework/Diagnostics/WorldStatsConsole.cs
@@ -1,4 +1,4 @@
-﻿﻿using System.Linq;
+﻿using System.Linq;
 using Snake.Framework.Behaviors;
 using Snake.Framework.Geometry;
 using Snake.Framework.Graphics;
@@ -6,51 +6,53 @@ using Snake.Framework.Physics;
 
 namespace Snake.Framework.Diagnostics
 {
-	/// <summary>
-	/// A console to show stats by game world.
-	/// </summary>
-	public class WorldStatsConsole : ComponentBase, IUpdatable, ISceneSurvivable
-	{
-		private readonly Point position;
+    /// <summary>
+    /// A console to show stats by game world.
+    /// </summary>
+    public class WorldStatsConsole : ComponentBase, IUpdatable, ISceneSurvivable
+    {
+        private readonly Point position;
 
-		private WorldStatsConsole(int x, int y, IWorldContext context)
+        private WorldStatsConsole(int x, int y, IWorldContext context)
             : base(context)
-		{
-			this.position = new Point(x, y);
-		}
+        {
+            this.position = new Point(x, y);
+        }
 
         public static WorldStatsConsole Create(int x, int y, IWorldContext context)
         {
-            return new WorldStatsConsole(x, y, context);    
+            return new WorldStatsConsole(x, y, context);
         }
 
-		public void Update()
-		{
-			var components = Context.Components;
-			var enabledComponentsCount = components.Count(c => c.Enabled);
-			var disabledComponentsCount = components.Count(c => !c.Enabled);
-			var updatablesCount = components.Count(c => c is IUpdatable);
-			var drawablesCount = components.Count(c => c is IDrawable);
-			var collidablesCount = components.Count(c => c is ICollidable);
-			var sceneSurvivablescount = components.Count(c => c is ISceneSurvivable);
+        public void Update()
+        {
+            var components = Context.Components;
+            var enabledComponentsCount = components.Count(c => c.Enabled);
+            var disabledComponentsCount = components.Count(c => !c.Enabled);
+            var updatablesCount = components.Count(c => c is IUpdatable);
+            var drawablesCount = components.Count(c => c is IDrawable);
+            var collidablesCount = components.Count(c => c is ICollidable);
+            var sceneSurvivablescount = components.Count(c => c is ISceneSurvivable);
 
-			var x = position.X;
-			var y = position.Y;
-			var ts = Context.TextSystem;
+            var x = position.X;
+            var y = position.Y;
+            var ts = Context.TextSystem;
 
-			ts.Draw(x, y,   "Components                 : {0}".With(components.Count), "Debug");
-			ts.Draw(x, ++y, "Enabled components         : {0}".With(enabledComponentsCount), "Debug");
-			ts.Draw(x, ++y, "Disabled components        : {0}".With(disabledComponentsCount), "Debug");
+            ts
+                .Draw(x, y, "FPS                        : {0:N0}".With(1f / Context.Time.SinceLastFrame), "Debug")
+                .Draw(x, ++y, "Components                 : {0}".With(components.Count), "Debug")
+                .Draw(x, ++y, "Enabled components         : {0}".With(enabledComponentsCount), "Debug")
+                .Draw(x, ++y, "Disabled components        : {0}".With(disabledComponentsCount), "Debug")
+                .Draw(x, ++y, "Updatable components       : {0}".With(updatablesCount), "Debug")
+                .Draw(x, ++y, "Drawable components        : {0}".With(drawablesCount), "Debug")
+                .Draw(x, ++y, "Collidable components      : {0}".With(collidablesCount), "Debug")
+                .Draw(x, ++y, "Scene survivable components: {0}".With(sceneSurvivablescount), "Debug");
 
-			ts.Draw(x, ++y, "Updatable components       : {0}".With(updatablesCount), "Debug");
-			ts.Draw(x, ++y, "Drawable components        : {0}".With(drawablesCount), "Debug");
-			ts.Draw(x, ++y, "Collidable components      : {0}".With(collidablesCount), "Debug");
-			ts.Draw(x, ++y, "Scene survivable components: {0}".With(sceneSurvivablescount), "Debug");
-		}
+        }
 
-		public bool CanSurvive(IScene fromScene, IScene toScene)
-		{
-			return true;
-		}
-	}
+        public bool CanSurvive(IScene fromScene, IScene toScene)
+        {
+            return true;
+        }
+    }
 }
