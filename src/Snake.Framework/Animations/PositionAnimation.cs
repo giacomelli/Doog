24f2e61edc1a3b9c8e4c550342b6eb7aef15aad9
giacomelli <giacomelli@gmail.com>
2017-08-24 19:18:58 -0300

    Refactoring PositionAnimation, there is no need to keep diff values to from/to forward and backward directions.

--- src/Snake.Framework/Animations/PositionAnimation.cs
+++ src/Snake.Framework/Animations/PositionAnimation.cs
@@ -8,86 +8,39 @@ namespace Snake.Framework.Animations
     /// </summary>
     public class PositionAnimation : AnimationBase<Transform>
     {
-        private Point? fromForward;
-        private Point toForward;
-
-        private Point? fromBackward;
-		private Point toBackward;
+        private Point to;
+        private Point from;
 
         public PositionAnimation(Transform owner, Point to, float duration)
             : base(owner, duration)
         {
-            if (Direction == AnimationDirection.Backward)
-            {
-                this.toBackward = to;
-            }
-            else 
-            {
-                this.toForward = to;
-            }
+            this.to = to;
         }
 
         public override void Play()
         {
-            if (Direction == AnimationDirection.Backward)
-            {
-                if (!fromBackward.HasValue)
-                {
-                    fromBackward = Owner.Position;
-                }
-            }
-            else if (!fromForward.HasValue) 
-			{
-				fromForward = Owner.Position;
-			}
-
+            this.from = Owner.Position;
             base.Play();
         }
 
         protected override void UpdateValue(float time)
         {
-			if (Direction == AnimationDirection.Backward)
-			{
-				Owner.Position = new Point(
-				Easing.Calculate(fromBackward.Value.X, toBackward.X, time),
-				Easing.Calculate(fromBackward.Value.Y, toBackward.Y, time));
-			}
-			else
-			{
-				Owner.Position = new Point(
-				Easing.Calculate(fromForward.Value.X, toForward.X, time),
-				Easing.Calculate(fromForward.Value.Y, toForward.Y, time));
-			}
-        }
+			Owner.Position = new Point(
+			     Easing.Calculate(from.X, to.X, time),
+			     Easing.Calculate(from.Y, to.Y, time));
+		}
 
         public override void Reset()
         {
-			if (Direction == AnimationDirection.Backward)
-			{
-                Owner.Position = fromBackward.Value;
-			}
-			else
-			{
-                Owner.Position = fromForward.Value;
-			}
-
+            Owner.Position = from;
             base.Reset();
         }
 
         public override void Reverse()
         {
-			if (Direction == AnimationDirection.Backward)
-			{
-                var temp = fromBackward.Value;
-				fromBackward = toBackward;
-				toBackward = temp;
-			}
-			else
-			{
-				var temp = fromForward.Value;
-				fromForward = toForward;
-				toForward = temp;
-			}
-        }
+			var temp = from;
+			from = to;
+			to = temp;
+		}
     }
 }
