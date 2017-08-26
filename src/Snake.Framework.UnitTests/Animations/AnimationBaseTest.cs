ba6f45515d819040d87542503400c0f875925cfe
giacomelli <giacomelli@gmail.com>
2017-08-26 12:20:31 -0300

    Improving code coverage

--- src/Snake.Framework.UnitTests/Animations/AnimationBaseTest.cs
+++ src/Snake.Framework.UnitTests/Animations/AnimationBaseTest.cs
@@ -72,5 +72,52 @@ namespace Snake.Framework.UnitTests.Animations
             var target = new FloatAnimation<Transform>(owner, 1, 3, 5, v => {});
             Assert.AreEqual("FloatAnimation<Transform>(1..3 in 5s)", target.ToString());
         }
+
+		[Test]
+		public void Reverse_FromTo_ToFrom()
+		{
+			var sinceSceneStart = 0f;
+			var ctx = MockRepository.GenerateMock<IWorldContext>();
+			ctx.Expect(t => t.LogSystem).Return(MockRepository.GenerateMock<ILogSystem>());
+
+			var time = MockRepository.GenerateMock<ITime>();
+			time.Expect(t => t.SinceSceneStart).WhenCalled(m =>
+			{
+				m.ReturnValue = sinceSceneStart;
+			}).Return(0);
+			ctx.Expect(t => t.Time).Return(time);
+
+			var owner = new Transform(ctx);
+			float currentValue = 0;
+			var target = new FloatAnimation<Transform>(owner, 1, 3, 5, v => currentValue = v);
+
+			var hasStarted = false;
+			var hasEnded = false;
+			target.Started += (sender, e) => hasStarted = true;
+			target.Ended += (sender, e) => hasEnded = true;
+
+            target.Reverse();
+			target.Play();
+			sinceSceneStart = 0;
+			target.Update();
+			Assert.AreEqual(3, currentValue);
+			Assert.IsTrue(hasStarted);
+			Assert.IsFalse(hasEnded);
+
+			sinceSceneStart = 2.5f;
+			target.Update();
+			Assert.AreEqual(2, currentValue);
+
+            sinceSceneStart = 5f;
+			target.Update();
+			Assert.AreEqual(1, currentValue);
+
+			sinceSceneStart = 5.51f;
+			target.Update();
+			Assert.AreEqual(1, currentValue);
+
+			Assert.AreEqual(AnimationState.Stopped, target.State);
+			Assert.IsTrue(hasEnded);
+		}
     }
 }
