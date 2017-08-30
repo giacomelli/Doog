3428322c254e57ce563420c9cad622dd3f8a8a91
giacomelli <giacomelli@gmail.com>
2017-08-29 22:57:04 -0300

    Improving code coverage

--- src/Snake.Framework/Geometry/TransformExtensions.cs
+++ src/Snake.Framework/Geometry/TransformExtensions.cs
@@ -1,8 +1,15 @@
-﻿using System;
-namespace Snake.Framework.Geometry
+﻿namespace Snake.Framework.Geometry
 {
+    /// <summary>
+    /// Transform extensions methods.
+    /// </summary>
     public static class TransformExtensions
     {
+        /// <summary>
+        /// Centralizes the pivot.
+        /// </summary>
+        /// <returns>The pivot.</returns>
+        /// <param name="transform">Transform.</param>
         public static Transform CentralizePivot(this Transform transform)
         {
             transform.Pivot = Point.HalfOne;
