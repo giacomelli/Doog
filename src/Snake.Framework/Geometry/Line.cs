ad8986583eb5e61199001c70c60c852c29fa868e
giacomelli <giacomelli@gmail.com>
2017-09-03 15:18:08 -0300

    Create ILine, Line, LineComponent and extensions

---
+++ src/Snake.Framework/Geometry/Line.cs
@@ -0,0 +1,38 @@
+﻿namespace Snake.Framework.Geometry
+{
+    /// <summary>
+    /// An immutable line.
+    /// </summary>
+    public struct Line : ILine
+    {
+        private readonly Point pointA;
+        private readonly Point pointB;
+     
+        public Line(Point pointA, Point pointB)
+        {
+            this.pointA = pointA;
+            this.pointB = pointB;
+        }
+
+        public Line(float pointAX, float pointAY, float pointBX, float pointBY)
+            : this (new Point(pointAX, pointAY), new Point(pointBX, pointBY))
+        {
+        }
+
+		public Point PointA
+		{
+			get
+			{
+				return pointA;
+			}
+		}
+
+		public Point PointB
+		{
+			get
+			{
+				return pointB;
+			}
+		}
+    }
+}
